7414a67a678b369757a221f1bbcff20b10b93db8
Rakesh R <rakesh2693@gmail.com>
2020-07-21 15:22:24 -0700

    Initial Commit

---
+++ tipsy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Rakesh R on 7/21/20.
+//  Copyright © 2020 Rakesh R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
