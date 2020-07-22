fe4f77e66a219665cb00bbfd8d74f3c3d41cb47b
Rakesh R <rakesh2693@gmail.com>
2020-07-21 17:39:19 -0700

    Changes to calculate tip

--- tipsy/ViewController.swift
+++ tipsy/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    
+    @IBOutlet weak var peopleSlider: UISlider!
+    
+    @IBOutlet weak var shareLabel: UILabel!
+    
+    @IBOutlet weak var numPeopleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let numberOfPeople = Float(lroundf(peopleSlider.value))
+        peopleSlider.setValue(numberOfPeople, animated: true)
+        let bill = Float(billField.text!) ?? 0
+        let tip = bill * tipSlider.value * 0.01
+        let total = bill + tip
+        tipPercentLabel.text = String.init(format: "%.2f", tipSlider.value) + "%"
+        tipLabel.text = String(format: "$ %.2f", tip)
+        totalLabel.text = String(format: "$ %.2f", total)
+        numPeopleLabel.text = String(Int(numberOfPeople))
+        shareLabel.text = String(format: "$ %.2f", total/numberOfPeople)
+    }
+    
+    
 }
 
